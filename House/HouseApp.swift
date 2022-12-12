861b12df53fbb40a8b50d31aa5d27226504f50fc
Kirill Ki <kirillkalapov@gmail.com>
2022-12-12 13:24:42 +0100

    Initial Commit

---
+++ House/HouseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HouseApp.swift
+//  House
+//
+//  Created by  Mr.Ki on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct HouseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
