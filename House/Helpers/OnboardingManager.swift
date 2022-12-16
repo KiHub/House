f98210c02fd097757d65f119c9997d5bb0fd0536
Kirill Ki <kirillkalapov@gmail.com>
2022-12-16 10:40:55 +0100

    Completed project

---
+++ House/Helpers/OnboardingManager.swift
@@ -0,0 +1,33 @@
+//
+//  OnboardingManager.swift
+//  House
+//
+//  Created by  Mr.Ki on 15.12.2022.
+//
+
+import SwiftUI
+
+struct OnboardingManager: View {
+    @State private var currentTab = 0
+    var body: some View {
+        
+        TabView(selection: $currentTab,
+                content:  {
+            ForEach(OnboardingData.list) { viewData in
+               
+                OnboardingView(data: viewData)
+                    .tag(viewData.id)
+            }
+        })
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    }
+}
+
+
+struct OnboardingManager_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingManager()
+    }
+}
+
